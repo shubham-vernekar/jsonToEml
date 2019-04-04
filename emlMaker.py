3b0690ff5f24f74ffdbdd08aff260b9872497c18
Shubham Vernekar <vernekar_s@India.XoriantCorp.com>
2019-04-04 13:06:20 +0530

    Added support to accept attachments in base64 and multiple email recepiants

--- emlMaker.py
+++ emlMaker.py
@@ -9,20 +9,34 @@ def generateBoundary(stringLength=10):
     return "_" + ''.join(random.choice(letters) for i in range(stringLength)) + "_"
 
 def trimString(data, key, length):
-    ans = ""
+    result = ""
     count = 0
     for char in data:
-        ans += char
+        result += char
         count += 1
 
         if count >= length:
-            ans += key
+            result += key
             count = 0
 
         if char == "\n":
             count = 0
 
-    return ans
+    return result
+
+def nameIDFormat(data):
+    result = ""
+    for i in data:
+        name = i.get("name", "")
+        email = i.get("email", "")
+        if name:
+            result += '{} <{}>; '.format(name,email)
+        else:
+            result += '"{}" <{}>; '.format(email,email)
+    return result[:-2]
+
+def b64Size(b64string):
+    return (len(b64string) * 3) / 4 - b64string.count('=', -2)
 
 class emlMaker:
     def __init__(self):
@@ -51,9 +65,13 @@ class emlMaker:
                 emlString += "{}: {}".format(key, headers[key]) + "\n"
 
         if emailFrom:
-            emlString += "From: {}\n".format(emailFrom)
+            emlString += "From: {}\n".format(nameIDFormat([emailFrom]))
         if emailTo:
-            emlString += "To: {}\n".format(emailTo)
+            emlString += "To: {}\n".format(nameIDFormat(emailTo))
+        if cc:
+            emlString += "Cc: {}\n".format(nameIDFormat(cc))
+        if bcc:
+            emlString += "Bcc: {}\n".format(nameIDFormat(bcc))
         if subject:
             emlString += "Subject: {}\n".format(subject)
         if timestamp:
@@ -82,16 +100,22 @@ class emlMaker:
             emlString += "\n--{}--\n".format("_000" + boundary)
 
         for attachment in attachments:
-            filename = os.path.basename(attachment)
-            with open(attachment,'rb') as file:
-                base64Data = base64.b64encode(file.read())
-                base64Data = trimString(base64Data.decode('utf-8'), "\n", 76)
+            if attachment.get("filename",False):
+                filename = attachment.get("filename","")
+                basename = os.path.basename(filename)
+                with open(filename,'rb') as file:
+                    base64Data = base64.b64encode(file.read())
+                    base64Data = trimString(base64Data.decode('utf-8'), "\n", 76)
+                fileSize = os.path.getsize(filename)
+            else:
+                basename = attachment.get("name","")
+                base64Data = trimString(attachment.get("raw",""), "\n", 76)
+                fileSize = b64Size(base64Data)
 
-            mimeType = MimeTypes().guess_type(attachment)[0]
-            fileSize = os.path.getsize(attachment)
+            mimeType = MimeTypes().guess_type(basename)[0]
 
             emlString += '\n--{}\nContent-Type: {}; name="{}"\nContent-Description: {}\n\
-Content-Disposition: attachment; filename="{}"; size={}\nContent-Transfer-Encoding: base64\n\n'.format("_004" + boundary,mimeType,filename,filename,filename,fileSize)
+Content-Disposition: attachment; filename="{}"; size={}\nContent-Transfer-Encoding: base64\n\n'.format("_004" + boundary,mimeType,basename,basename,basename,fileSize)
             emlString += "{}\n".format(base64Data)
 
         emlString += "\n--{}--\n".format("_004" + boundary)
